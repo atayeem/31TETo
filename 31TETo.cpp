0a8f2ae214a5487573669d26a0387c89b43a64ab
atayeem <aseertayeem@gmail.com>
2025-11-04 18:46:27 -0500

    add more configuration logic and json reading

--- 31TETo.cpp
+++ 31TETo.cpp
@@ -1,25 +1,61 @@
+#include <algorithm>
 #include <array>
 #include <cstdint>
 #include <iostream>
 #include <fstream>
+#include <math.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <string>
 #include <vector>
 #include <filesystem>
-#include <unordered_map>
+#include "cJSON.h"
 
-def note_to_midi(n):
-    m = note_re.match(n)
-    if not m: raise ValueError(f"Bad note '{n}'")
-    nm, octv = m.groups()
-    return (int(octv)+1)*12 + notes[nm]
+float midi_to_cents_12edo(int midi_note) {
+    return 100 * (midi_note - 69);
+}
 
-def midi_to_hz(m):
-    return 440.0 * 2**((m-69)/12)
-
+// This the kind of code they would write for the PDP-11
 int note_to_midi(std::string note) {
-    static std::unordered_map<std::string, int> map; 
+    const char *s = note.c_str();
+    int n, octv, sgn;
+    switch (s[0]) {
+        case 'C': n = 0; break;
+        case 'D': n = 2; break;
+        case 'E': n = 4; break;
+        case 'F': n = 5; break;
+        case 'G': n = 7; break;
+        case 'A': n = 9; break;
+        case 'B': n = 11; break;
+    }
+
+    // A-1
+    if (s[1] == '-') {
+        sgn = -1;
+        octv = s[3] - '0';
+    }
+
+    else if (s[1] == '#') {
+        
+        // A#-1
+        if (s[2] == '-') {
+            sgn = -1;
+            octv = s[3] - '0';
+        }
+        // A#1
+        else {
+            sgn = 1;
+            octv = s[2] - '0';
+        }
+    }
+
+    // A1
+    else {
+        sgn = 1;
+        octv = s[1] - '0';
+    }
+
+    return (sgn * octv + 1) * 12 + n;
 }
 
 struct Config {
@@ -33,30 +69,116 @@ struct Config {
     /* How much the `Z` integer flag changes the note. */
     float StepSize;
 
-    /* Value of A4. */
+    /* For example, standard tuning is ReferencePitch=440.0 */
     float ReferencePitch;
+    std::string ReferencePitchName;
 
+    std::vector<std::string> ExecPath;
 
-    std::vector<std::string> exec_path;
+private:
+    /* Calculate scale degree in cents */
+    float scl(int n) {
+        int scale_steps = Scale.size() - 1;
+        int octave = n / scale_steps;
+        int degree = n % scale_steps;
+        float equave = Scale.back();
 
+        return Scale[degree] + octave * equave;
+    }
+
+    /* I don't know how this works. */
+    void generate_mapping() {
+        int ref = note_to_midi(ReferencePitchName);
+        for (int i = 0; i < 132; i++) {
+            mapping[i] = 1200 * log2(ReferencePitch/440.0) + 100 * (69 - ref) + scl(i) - midi_to_cents_12edo(i);
+        }
+    }
+
+    void defconfig() {
+        ReferencePitch = 440.0;
+        StepSize = 1200.0 / 31.0;
+        
+        // 31-edo chromatic with # meaning +2 (meantone)
+        float steps[11] = {2, 5, 7, 10, 13, 15, 18, 20, 23, 25, 28};
+
+        std::for_each_n(steps, sizeof(steps) / sizeof(float), [](float &n){n *= 1200.0/31;});
+        std::copy_n(steps, 12, Scale);
+    }
+public:
     Config(std::filesystem::path config_path) {
 
         if (!std::filesystem::exists(config_path)) {
             std::ofstream config_file(config_path);
             if (!config_file) {
-                std::cerr << "[31TETo] Failed to create config file. defaulting to 31edo.\n";
+                std::cout << "[31TETo] JSON config could not be created at " << config_path << "\n";
+                
             } else {
-                std::cout << "[31TETo] Created config file at " << config_path << "\n";
-                config_file << "ReferencePitch=450.0\n";
-                config_file << "Scale=[31ED2]\n";
-                config_file << "StepSize=[1\\31]\n";
-                config_file << "Resampler=wine,moresampler.exe\n";
-
+                std::cout << "[31TETo] Created new config file at " << config_path << "\n";
                 config_file.close();
             }
-        } else {
+            std::cout << "[31TETo] Continuing with defconfig\n";
+            defconfig();
 
+        } else {
+            std::ifstream config_file(config_path);
+            std::string contents(std::istreambuf_iterator<char>(config_file), {});
+
+            cJSON* data = cJSON_Parse(contents.c_str());
+
+            if (!data) {
+                std::cout << "[31TETo] JSON config at " << config_path << " is malformed: " << cJSON_GetErrorPtr() << "\n";
+                std::cout << "[31TETo] Continuing with defconfig.\n";
+                defconfig();
+            }
+
+            else {
+                const cJSON* focus = cJSON_GetObjectItemCaseSensitive(data, "StepSize");
+                if (cJSON_IsNumber(focus)) {
+                    StepSize = focus->valuedouble;
+                } else {
+                    std::cout << "[31TETo] StepSize isn't a number.\n";
+                    StepSize = pow(2, 1/31);
+                }
+
+                focus = cJSON_GetObjectItemCaseSensitive(data, "ReferencePitch");
+                if (cJSON_IsNumber(focus)) {
+                    ReferencePitch = focus->valuedouble;
+                } else {
+                    std::cout << "[31TETo] ReferencePitch isn't a number.\n";
+                    ReferencePitch = 440.0;
+                }
+
+                focus = cJSON_GetObjectItemCaseSensitive(data, "ReferencePitchName");
+                if (cJSON_IsString(focus) && (focus->valuestring != nullptr) ) {
+                    ReferencePitchName = cJSON_GetStringValue(focus);
+                } else {
+                    std::cout << "[31TETo] ReferencePitchName isn't a string.\n";
+                    ReferencePitchName = "A";
+                }
+
+                focus = cJSON_GetObjectItemCaseSensitive(data, "Scale");
+                if (cJSON_IsArray(focus)) {
+                    const cJSON* degree;
+                    cJSON_ArrayForEach(degree, focus) {
+                        if (cJSON_IsNumber(degree)) {
+                            Scale.push_back(degree->valuedouble);
+                        } else {
+                            std::cout << "[31TETo] element of Scale array isn't a number.\n";
+                        }
+                    }
+                } else if (cJSON_IsString(focus) && (focus->valuestring != nullptr)) {
+                    std::cout << "[31TETo] Scale is a string: Opening file to get scale not yet supported.\n";
+                } else {
+                    std::cout << "[31TETo] Scale is not a string or array. Continuing with defconfig.\n";
+                    defconfig();
+                }
+
+                cJSON_Delete(data);
+            }
+            config_file.close();
         }
+
+        generate_mapping();
     }
 };
 
@@ -125,7 +247,7 @@ static std::vector<int16_t> pitch_string_to_cents(const std::string& in) {
             if (in[i] == '#') {
                 state = 1;
                 for (int i = 0; i < reps - 1; i++)
-                    out.push_back(out[out.size() - 1]);
+                    out.push_back(out.back());
                 
             }
             else {
@@ -182,10 +304,6 @@ static std::string cents_to_pitch_string(const std::vector<int16_t>& in) {
         out_str += last_pair + "#" + std::to_string(cnt) + "#";
 
     return out_str;
-}
-
-static void load_config(Config &cfg) {
-
 }
 
 int main(int argc, char *argv[]) {
